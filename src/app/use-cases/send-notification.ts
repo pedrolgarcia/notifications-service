2fcff403db5e881c338c32560181404291700ec9
Pedro Lopes <pedrolopesgrc@gmail.com>
2022-12-18 19:34:24 -0300

    feat: prisma repository, structuring project

--- src/app/use-cases/send-notification.ts
+++ src/app/use-cases/send-notification.ts
@@ -1,3 +1,4 @@
+import { Injectable } from '@nestjs/common';
 import { Content } from '../entities/content';
 import { Notification } from '../entities/notification';
 
@@ -13,6 +14,7 @@ interface SendNotificationResponse {
   notification: Notification;
 }
 
+@Injectable()
 export class SendNotification {
   constructor(private notificationsRepository: NotificationsRepository) {}
 
