6f68034a016b80a0af77163981fd5e83d05d0499
Pedro Lopes <pedrolopesgrc@gmail.com>
2022-12-18 21:22:52 -0300

    feat: cancel notification use case

---
+++ src/application/use-cases/cancel-notification.spec.ts
@@ -0,0 +1,42 @@
+import { CancelNotification } from './cancel-notification';
+
+import { Notification } from '@application/entities/notification';
+import { Content } from '@application/entities/content';
+
+import { NotificationNotFound } from '@application/use-cases/errors/notification-not-found';
+
+import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
+
+describe('Cancel notification', () => {
+  it('should be able to cancel a notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const cancelNotification = new CancelNotification(notificationsRepository);
+
+    const notification = new Notification({
+      category: 'social',
+      content: new Content('Nova solicitação de amizade!'),
+      recipientId: 'example-recipient-id',
+    });
+
+    await notificationsRepository.create(notification);
+
+    await cancelNotification.execute({
+      notificationId: notification.id,
+    });
+
+    expect(notificationsRepository.notifications[0].canceledAt).toEqual(
+      expect.any(Date),
+    );
+  });
+
+  it('should not be able to cancel a non existing notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const cancelNotification = new CancelNotification(notificationsRepository);
+
+    expect(() => {
+      return cancelNotification.execute({
+        notificationId: 'fake-notification-id',
+      });
+    }).rejects.toThrow(NotificationNotFound);
+  });
+});
